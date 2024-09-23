fb17ac8f6da65c8d76cfda28c697b7857cbc0d82
kvederaviciute <jovitakved@gmail.com>
2024-09-23 19:57:05 +0300

    Update studentas.h

--- studentas.h
+++ studentas.h
@@ -6,9 +6,11 @@
 struct Stud{
     string vardas, pavarde;
     vector<int> ND;
-    int egzaminas;
+    double galutinis, egzaminas, vidurkis, mediana;
 };
 
 void ivedimas(Stud & student,int m);
+void valymas(Stud & student);
+void isvedimasgal(vector<Stud> student,int n);
 
 #endif // STUDENTAS_H_INCLUDED
