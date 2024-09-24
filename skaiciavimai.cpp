ac9cec3e6543b9b5bf03fb64df56dfd24b3a5f8f
kvederaviciute <jovitakved@gmail.com>
2024-09-24 18:51:14 +0300

    Update skaiciavimai.cpp
    pridėtos medianos skaičiavimo ir galutinio rezultato su mediana skaičiavimo funkcijos

--- skaiciavimai.cpp
+++ skaiciavimai.cpp
@@ -7,6 +7,20 @@ void vidurkis(Stud & student, int m){
     }
     student.vidurkis=sum/m;
 }
-void galutinis(Stud & student){
+void galutinisvid(Stud & student){
     student.galutinis = student.vidurkis*0.4 + student.egzaminas*0.6;
+}
+void mediana(Stud & student, int m){
+    double d=2;
+    vector<int> nd = student.ND;
+    sort(nd.begin(), nd.end());
+    if(m%2==0){
+        student.mediana = (nd[m / 2 - 1] + nd[m / 2]) / d;
+    }
+    else{
+        student.mediana = nd[m/2];
+    }
+}
+void galutinismed(Stud & student){
+    student.galutinismed = student.mediana*0.4 + student.egzaminas*0.6;
 }
