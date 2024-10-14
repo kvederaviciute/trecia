7c4270295dc92ab0035e7df9664a51646bdf9a33
kvederaviciute <jovitakved@gmail.com>
2024-10-14 14:44:37 +0300

    Update skaiciavimai.h

--- skaiciavimai.h
+++ skaiciavimai.h
@@ -9,5 +9,6 @@ void galutinisvid(Stud & student);
 void mediana(Stud & student, int m);
 void galutinismed(Stud & student);
 void rikiavimas(vector<Stud>& studentai);
+void skirstymas(vector<Stud> s, vector<Stud>& k, vector<Stud>& v);
 
 #endif // SKAICIAVIMAI_H_INCLUDED
