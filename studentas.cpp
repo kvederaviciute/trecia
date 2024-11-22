eb6806915c7e771bbb577db91a4c4492218b73fb
kvederaviciute <jovitakved@gmail.com>
2024-11-22 15:58:12 +0200

    Update studentas.cpp

--- studentas.cpp
+++ studentas.cpp
@@ -1,187 +1,172 @@
 #include "studentas.h"
 
-void ivedimas(Stud & student, int & m)
-{
-    int a,d,eg=11;
-    cout<<"Vardas: ";
-    cin>>student.vardas;
-    cout<<"Pavarde: ";
-    cin>>student.pavarde;
-    cout<<"Ar norite atsitiktinai generuoti rezultatus?(0 - ne, 1 - taip): ";
-    cin>>a;
-    if(cin.fail())
-    {
-        cout<<"Buvo ivestas netinkamas simbolis"<<endl;
+void Student::Ivedimas(int& m, bool random) {
+    int a;
+    double eg = 11;
+
+    std::cout << "Vardas: ";
+    std::cin >> vardas;
+    std::cout << "Pavarde: ";
+    std::cin >> pavarde;
+
+    std::cout << "Ar norite atsitiktinai generuoti rezultatus? (0 - ne, 1 - taip): ";
+    std::cin >> a;
+
+    if (std::cin.fail()) {
+        std::cout << "Buvo ivestas netinkamas simbolis" << std::endl;
+        exit(1);
+    } else if (a == 0) {
+        if (m == 0) {
+            NdBe(m);
+        } else {
+            NdSu(m);
+        }
+
+        while (eg < 0 || eg > 10) {
+            std::cout << "Egzamino rezultatas: ";
+            std::cin >> eg;
+            if (std::cin.fail()) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "Buvo ivestas netinkamas simbolis" << std::endl;
+                eg = 11;
+            } else if (eg >= 0 && eg <= 10) {
+                egzaminas = eg;
+            } else {
+                std::cout << "Egzamino rezultatas turi buti desimtbaleje sistemoje" << std::endl;
+            }
+        }
+    } else if (a == 1) {
+        if (m == 0) {
+            std::cout << "Kiek namu darbu norite sugeneruoti: ";
+            std::cin >> m;
+        }
+
+        NdRandom(m);
+    } else {
+        std::cout << "Skaicius turi buti arba 0 arba 1" << std::endl;
         exit(1);
     }
-    else if(a==0)
-    {
-        if(m==0)
-        {
-            namudarbaibe(student,m);
-        }
-        else
-        {
-            namudarbaisu(student,m);
-        }
-        while(eg<0 || eg>10)
-        {
-            cout<<"Egzamino rezultatas: ";
-            cin>>eg;
-            if(cin.fail())
-            {
-                cin.clear();
-                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                cout<<"buvo ivestas netinkamas simbolis"<<endl;
-                eg=11;
-            }
-            else if(eg>=0 && eg<=10)
-            {
-                student.egzaminas=eg;
-            }
-            else
-            {
-                cout<<"Egzamino rezultatas turi buti desimtbaleje sistemoje"<<endl;
-            }
-        }
-    }
-    else if(a==1)
-    {
-        if(m==0)
-        {
-            cout<<"Kiek namu darbu norite sugeneruoti: ";
-            cin>>m;
-            for(int i=0; i<m; i++)
-            {
-                d=rand() % 10 + 1;
-                student.ND.push_back(d);
-            }
-            student.egzaminas=rand() % 10 +1;
-        }
-        else
-        {
-            for(int i=0; i<m; i++)
-            {
-                d=rand() % 10 + 1;
-                student.ND.push_back(d);
-            }
-            student.egzaminas=rand() % 10 +1;
-        }
-    }
-    else
-    {
-        cout<<"skaicius turi buti arba 0 arba 1"<<endl;
-        exit(1);
-    }
-    cout <<"Studento saugojamo atmintyje adresas: "<< &student <<endl;
 }
 
-void namudarbaibe(Stud & student, int & m)
-{
-    int n=11;
-    while(n<0 || n>10)
-    {
-        cout<<"Iveskite "<<m+1<<"-uju namu darbu rezultata (noredami baigti ivedima, iveskite skaiciu -1): ";
-        cin>>n;
-        if(cin.fail())
-        {
-            cin.clear();
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            cout<<"buvo ivestas netinkamas simbolis"<<endl;
-            n=11;
+void Student::NdBe(int& m) {
+    int nd = 11;
+    while (nd != -1) {
+        std::cout << "Iveskite namu darbu rezultata (noredami baigti, iveskite -1): ";
+        std::cin >> nd;
+
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Buvo ivestas netinkamas simbolis" << std::endl;
+            continue;
         }
-        else if(n>=0 && n<=10)
-        {
-            student.ND.push_back(n);
-            m++;
-            n=11;
-        }
-        else if(n==-1)
-        {
-            break;
-        }
-        else
-        {
-            cout<<"Egzamino rezultatas turi buti desimtbaleje sistemoje"<<endl;
+
+        if (nd == -1) break;
+
+        if (nd >= 0 && nd <= 10) {
+            ND.push_back(nd);
+        } else {
+            std::cout << "Rezultatas turi buti desimtbaleje sistemoje" << std::endl;
         }
     }
+    m=ND.size();
 }
-void namudarbaisu(Stud & student, int m)
-{
-    int n=11;
-    for(int i=0; i<m; i++)
-    {
-        while(n<0 || n>10)
-        {
-            cout<<"Iveskite "<<i+1<<"-uju namu darbu rezultata (noredami baigti ivedima, iveskite skaiciu -1): ";
-            cin>>n;
-            if(cin.fail())
-            {
-                cin.clear();
-                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                cout<<"buvo ivestas netinkamas simbolis"<<endl;
-                n=11;
-            }
-            else if(n>=0 && n<=10)
-            {
-                student.ND.push_back(n);
-                n=11;
-                break;
-            }
-            else if(n==-1)
-            {
-                break;
-            }
-            else
-            {
-                cout<<"Egzamino rezultatas turi buti desimtbaleje sistemoje"<<endl;
+
+void Student::NdSu(int m) {
+    int n;
+    for (int i = 0; i < m; ++i) {
+            std::cout << "Iveskite " << i + 1 << "-uju namu darbu rezultata: ";
+            std::cin >> n;
+            if (std::cin.fail()) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "Buvo ivestas netinkamas simbolis" << std::endl;
+            } else if (n >= 0 && n <= 10) {
+                ND.push_back(n);
+            } else {
+                std::cout << "Rezultatas turi buti desimtbaleje sistemoje" << std::endl;
             }
         }
     }
 
-}
-void valymas(Stud & student)
-{
-    student.vardas.clear();
-    student.pavarde.clear();
-    student.ND.clear();
-}
-void isvedimasgal(vector<Stud> student,int n,string m)
-{
-    ofstream out(m);
-    out<<left<<setw(15)<<"Vardas "<<left<<setw(20)<<"Pavarde "<<left<<setw(18)<<"Galutinis (Vid.) "<<left<<setw(18)<<"Galutinis (Med.) "<<endl;
-    out<<"--------------------------------------------------------------------"<<endl;
-    for(int i=0; i<n; i++)
-    {
-        out<<left<<setw(15)<<student.at(i).vardas<<left<<setw(20)<<student.at(i).pavarde<<left<<setw(18)<<fixed<<setprecision(2)<<student.at(i).galutinisvid<<left<<setw(18)<<fixed<<setprecision(2)<<student.at(i).galutinismed<<endl;
+void Student::NdRandom(int m) {
+    for (int i = 0; i < m; ++i) {
+        ND.push_back(rand() % 10 + 1);
     }
+    egzaminas = rand() % 10 + 1;
 }
-void generavimas(int n,string m)
-{
-    auto start = std::chrono::high_resolution_clock::now();
-    ofstream out(m);
-    int ndk = rand() % 20 + 1;
 
-    out << "Vardas Pavarde ";
-    for (int i = 1; i <= ndk; i++)
-    {
-        out << "ND" << i << " ";
+void Student::clearData() {
+    vardas.clear();
+    pavarde.clear();
+    ND.clear();
+}
+
+void Student::Isvedimas(const std::vector<Student>& students, const std::string& filename) {
+    std::ofstream outFile(filename);
+    if (!outFile.is_open()) {
+        std::cerr << "Klaida: Nepavyko atidaryti failo " << filename << std::endl;
+        return;
     }
-    out << "Egzaminas" << endl;
-    for (int i = 1; i <= n; i++)
-    {
-        out << "Vardas" << i << " Pavarde" << i << " ";
-        for (int j = 0; j < ndk; j++)
-        {
-            out << rand() % 10 + 1 << " ";
+
+    outFile << std::left << std::setw(20) << "Vardas" << std::setw(20) << "Pavarde"
+            << std::setw(15) << "Galutinis (Vid.)" << std::setw(15) << "Galutinis (Med.)" << std::endl;
+    outFile << std::string(70, '-') << std::endl;
+
+    for (const auto& student : students) {
+        outFile << std::left << std::setw(20) << student.getVardas()
+                << std::setw(20) << student.getPavarde()
+                << std::setw(15) << std::fixed << std::setprecision(2) << student.getGalutinisVid()
+                << std::setw(15) << student.getGalutinisMed() << std::endl;
+    }
+
+    outFile.close();
+    std::cout << "Duomenys sekmingai isvesti i faila " << filename << std::endl;
+}
+void Student::Rikiavimas(std::vector<Student>& studentai) {
+    std::sort(studentai.begin(), studentai.end(), [](const Student& a, const Student& b) {
+        if (a.galutinisvid == b.galutinisvid) {
+            return a.vardas < b.vardas;
         }
-        out << rand() % 10 + 1 << endl;
+        return a.galutinisvid > b.galutinisvid;
+    });
+}
+
+void Student::Skirstymas(std::vector<Student>& studentai, std::vector<Student>& vargsiukai) {
+    auto it = std::remove_if(studentai.begin(), studentai.end(), [](const Student& student) {
+        return student.galutinisvid < 5.0;
+    });
+
+    vargsiukai.insert(vargsiukai.end(), it, studentai.end());
+    studentai.erase(it, studentai.end());
+}
+void Student::Vidurkis() {
+    if (!ND.empty()) {
+        double sum = std::accumulate(ND.begin(), ND.end(), 0.0);
+        vidurkis = sum / ND.size();
+    } else {
+        vidurkis = 0.0;
     }
+}
 
-    out.close();
+void Student::GalutinisVid() {
+    galutinisvid = 0.4 * vidurkis + 0.6 * egzaminas;
+}
 
-    auto stop = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration = stop - start;
+void Student::Mediana() {
+    if (!ND.empty()) {
+        std::sort(ND.begin(), ND.end());
+        if (ND.size() % 2 == 0) {
+            mediana = (ND[ND.size() / 2 - 1] + ND[ND.size() / 2]) / 2.0;
+        } else {
+            mediana = ND[ND.size() / 2];
+        }
+    } else {
+        mediana = 0.0;
+    }
+}
 
-    cout<<m<<" failo kurimo laikas: "<<fixed<<setprecision(6)<<duration.count()<<endl;
+void Student::Galutinismed() {
+    galutinismed = 0.4 * mediana + 0.6 * egzaminas;
 }
