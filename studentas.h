fde628b0e9a49a6ce3fc145f8e8b2836c2978569
kvederaviciute <jovitakved@gmail.com>
2024-12-20 04:13:03 +0200

    Update studentas.h

--- studentas.h
+++ studentas.h
@@ -2,13 +2,10 @@
 #define STUDENTAS_H_INCLUDED
 
 #include "pagrindas.h"
-#include <iostream>
-#include <vector>
-#include <string>
 
 class Student {
 private:
-    std::string vardas = "Nezinomas", pavarde = "Nezinomas";
+    std::string vardas, pavarde;
     std::vector<int> ND;
     double egzaminas = 0;
     double vidurkis = 0;
@@ -17,49 +14,64 @@ private:
     double galutinismed = 0;
 
 public:
-// Constructor
+     // Default Constructor
+    Student() : vardas("Nezinomas"), pavarde("Studentas") {}
 
-Student(const std::string& vardas = "", const std::string& pavarde = "", double egzaminas = 0.0);
+    // Parameterized Constructor
+    Student(const std::string& vardas, const std::string& pavarde)
+        : vardas(vardas), pavarde(pavarde) {}
 
-// Copy Constructor
-Student(const Student& other)
-    : vardas(other.vardas), pavarde(other.pavarde), ND(other.ND), egzaminas(other.egzaminas),
-      vidurkis(other.vidurkis), mediana(other.mediana), galutinisvid(other.galutinisvid),
-      galutinismed(other.galutinismed){}
+    // Copy Constructor (Rule of Three)
+    Student(const Student& other);
 
-// Copy Assignment Operator
-Student& operator=(const Student& other) {
-    if (this == &other) return *this; // Handle self-assignment
-    vardas = other.vardas;
-    pavarde = other.pavarde;
-    ND = other.ND;
-    egzaminas = other.egzaminas;
-    vidurkis = other.vidurkis;
-    mediana = other.mediana;
-    galutinisvid = other.galutinisvid;
-    galutinismed = other.galutinismed;
+    // Copy Assignment Operator (Rule of Three)
+    Student& operator=(const Student& other);
 
-    return *this;
-}
+    // Destructor (Rule of Three)
+    ~Student();
 
-// Destructor
-~Student() {
-    ND.clear();
-}
-
-    // Setters
     void setVardas(const std::string& vardas) { this->vardas = vardas; }
     void setPavarde(const std::string& pavarde) { this->pavarde = pavarde; }
     void addNamudarbai(int nd) { ND.push_back(nd); }
     void setEgzaminas(double egzaminas) { this->egzaminas = egzaminas; }
 
-    // Getters
     std::string getVardas() const { return vardas; }
     std::string getPavarde() const { return pavarde; }
     double getGalutinisVid() const { return galutinisvid; }
     double getGalutinisMed() const { return galutinismed; }
 
-    // Member Functions
+    friend std::ostream& operator<<(std::ostream& os, const Student& student) {
+        os << std::left << std::setw(15) << student.vardas
+           << std::setw(20) << student.pavarde
+           << std::setw(10) << std::fixed << std::setprecision(2) << student.galutinisvid
+           << std::setw(10) << student.galutinismed;
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, Student& student) {
+        std::cout << "Įveskite vardą: ";
+        is >> student.vardas;
+        std::cout << "Įveskite pavardę: ";
+        is >> student.pavarde;
+
+        int nd;
+        std::cout << "Įveskite namų darbų pažymius (baigti su -1): ";
+        while (is >> nd && nd != -1) {
+            student.ND.push_back(nd);
+        }
+
+        std::cout << "Įveskite egzamino pažymį: ";
+        is >> student.egzaminas;
+
+        student.Vidurkis();
+        student.Mediana();
+        student.GalutinisVid();
+        student.Galutinismed();
+
+        return is;
+    }
+
+
     void Ivedimas(int& m, bool random = false);
     void Vidurkis();
     void GalutinisVid();
@@ -71,16 +83,10 @@ Student& operator=(const Student& other) {
     void NdRandom(int m);
     void clearData();
 
-    // Static Functions
     static void Rikiavimas(std::vector<Student>& studentai);
     static void Skirstymas1(std::vector<Student>& studentai, std::vector<Student>& vargsiukai, std::vector<Student>& kietiakai);
     static void Skirstymas2(std::vector<Student>& studentai, std::vector<Student>& vargsiukai);
     static void Skirstymas3(std::vector<Student>& studentai, std::vector<Student>& vargsiukai);
-
-    friend std::ostream& operator<<(std::ostream& os, const Student& student);
 };
-
-#endif // STUDENTAS_H_INCLUDED
-
 
 #endif // STUDENTAS_H_INCLUDED
